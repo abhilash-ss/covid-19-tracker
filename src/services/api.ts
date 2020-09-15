f1acb1d5286e4020be040e1b25ec05abbb4e76c9
abhilash ss <ss.abhilash3@gmail.com>
2020-09-15 23:48:25 +0530

    add getCovidInfo and getCovidInfoByCountryCode api

--- src/services/api.ts
+++ src/services/api.ts
@@ -9,4 +9,28 @@ const getCountries = async () => {
   }
 };
 
-export { getCountries };
+const getCovidInfo = async () => {
+  try {
+    const response = await fetch(`https://disease.sh/v3/covid-19/all`);
+    const data = await response.json();
+    return data;
+  } catch (err) {
+    console.log(err);
+    return [];
+  }
+};
+
+const getCovidInfoByCountryCode = async (countryCode: string) => {
+  try {
+    const response = await fetch(
+      `https://disease.sh/v3/covid-19/countries/${countryCode}`
+    );
+    const data = await response.json();
+    return data;
+  } catch (err) {
+    console.log(err);
+    return [];
+  }
+};
+
+export { getCountries, getCovidInfo, getCovidInfoByCountryCode };
